71eba73f8d7ebaa4ee420b19a8a2b76a385cbc67
Aman Chaudhary <amn234singh@gmail.com>
2025-06-17 03:35:18 +0530

    first commit

---
+++ app/companions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanionsLibrary = () => {
+  return (
+    <div>companions</div>
+  )
+}
+
+export default CompanionsLibrary 
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>yo sign in </div>
+  )
+}
+
+export default page
